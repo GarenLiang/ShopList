9e783cc283b20e0551f02f6165d75c6b168be1ae
GarenLiang <ideabreeze@gmail.com>
2017-07-03 23:23:22 -0700

    ViewList

--- ShopList/AllListsViewController.swift
+++ ShopList/AllListsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AllListsViewController.swift
 //  ShopList
 //
 //  Created by GarenLiang on 2017/7/3.
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AllListsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //Action
+    
+    @IBAction func addBarButtonItemPressed(_ sender: Any) {
+    }
 
-
-}
-
+    }
